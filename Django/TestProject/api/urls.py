351f7dcff2d1846a4f55734628a1f3db6c7c7caf
Aman Raj <compidemail@gmail.com>
2026-02-13 18:02:16 +0530

    added complete djnago notes

--- Django/TestProject/api/urls.py
+++ Django/TestProject/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import my_func, show, register, registration_success
+from .views import (my_func, show, register, registration_success, 
+setcookie, getcookie,delcookie, MyClassView1, another_my_func)
 urlpatterns = [
     path('a/', my_func, {'status':'OK'} ,name='my_func'),
     path('b/', show),
     path('c/', register),
-    path('d/', registration_success)
+    path('d/', registration_success),
+    path('set/', setcookie),
+    path('get',getcookie),
+    path('del',delcookie),
+    path('e/',MyClassView1.as_view(),name="myclassview1"),
+    path('f',another_my_func),
 ]
--- Django/TestProject/api/views.py
+++ Django/TestProject/api/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from api.models import Profile, User
 from .forms import Registration
 from django.http import HttpResponseRedirect
+from datetime import datetime,timedelta
+from django.views import View
+from django.http import JsonResponse
+from django.core.serializers import serialize
 # Create your views here.
 
 def my_func(request, **kwargs):
@@ -31,3 +35,31 @@ def register(request):
 
 def registration_success(request):
     return render(request, 'api/success.html')
+
+# server se client mai set kar rhe
+def setcookie(request):
+    response = render(request, 'api/setcookie.html')
+    response.set_cookie('cookie_iss_naam_se_jana_jayega', 'cookie_key_val_123', max_age=5)
+    return response
+
+# client se server mai kaise get karenge
+def getcookie(request):
+    response = render(request, 'api/getcookie.html')
+    #get_cookie_from_client = request.COOKIES['cookie_iss_naam_se_jana_jayega']
+    # when you want to see some deault value when cookie is delete then:
+    get_cookie_from_client = request.COOKIES.get('cookie_iss_naam_se_jana_jayega','default123')
+    print(get_cookie_from_client)
+    return response
+
+def delcookie(request):
+    response = render(request, 'api/delcookie.html')
+    response.delete_cookie('cookie_iss_naam_se_jana_jayega')
+    return response
+
+class MyClassView1(View):
+    def get(self,request):
+        return HttpResponse("Hellow from class based View")
+
+def another_my_func(request):
+    p = serialize('json',Profile.objects.all())
+    return JsonResponse(p,safe=False)
