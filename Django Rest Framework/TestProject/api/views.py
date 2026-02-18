a7f2484dd7e4134b0a516d5ca49dc426ef0f0888
Aman Raj <compidemail@gmail.com>
2026-02-18 23:39:53 +0530

    drf completed

--- Django Rest Framework/TestProject/api/views.py
+++ Django Rest Framework/TestProject/api/views.py
@@ -6,8 +6,66 @@ from django.http import HttpResponse, JsonResponse
 import io
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
+@api_view(['GET'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_detail(request):
+    stu = Student.objects.all()
+    serializer = StudentSerializer(stu, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_create(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'msg': 'data create ho gaya hai'}, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'PATCH', 'DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_update(request, pk): #/c/2: 2 is pk from url
+
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    # PUT → Full Update
+    if request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # PATCH → Partial Update
+    elif request.method == 'PATCH':
+        serializer = StudentSerializer(student, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # DELETE → Delete Student
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response({'msg': 'Student deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+'''
 def student_detail(request):
     stu = Student.objects.all()
     serializer = StudentSerializer(stu, many=True)
@@ -30,3 +88,4 @@ def student_create(request):
             return HttpResponse(json_data, content_type='application/json')
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type='application/json')
+'''
